c041a66f1c8e66ddbed748bc6bec9cd62af232cd
Jairama <jairam637@gmail.com>
2022-01-29 10:23:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleSubmit = (e) => {};
+
+  const handleReset = () => {};
+
+  const handleSearch = (e) => {
+    // log your value here
+  };
+
+  const debounce = (callback, delay) => {
+    if (restore) {
+      vm.search = "";
+    }
+    if (vm.promise) {
+      $timeout.cancel(vm.promise);
+    }
+    vm.promise = $timeout(function () {
+      vm.dtInstance.reloadData();
+    }, 800);
+  };
+    // add your debounce logic here
+  };
+  const debouncedSearch = debounce(handleSearch, 1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>
+        <p>part 1</p>
+        <label>
+          Name:
+          <input placeholder="name" type="text" />
+        </label>
+        <label>
+          Email:
+          <input placeholder="email" type="text" />
+        </label>
+
+
+        <label>
+          Password:
+          <input placeholder="password" type="text" />
+        </label>
+        <hr />
+        <button>Focus Name Input</button>
+        <button>Focus Email Input</button>
+        <button>Focus Password Input</button>
+        <hr />
+        <button>Submit</button>
+        <button>Reset</button>
+      </div>
+      <div>
+        <hr />
+        <p>part 2</p>
+        <label>
+          Search:
+          <input
+            placeholder="search with debounce"
+            type="text"
+            onChange={debouncedSearch}
+          />
+        </label>
+      </div>
+    </React.Fragment>
   );
 }
 
